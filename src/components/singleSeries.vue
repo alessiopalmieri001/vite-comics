e305dbae600c848d0e1657276a40ec3608332eb5
alessiopalmieri001 <alessiopalmieri001@gmail.com>
2024-01-19 22:58:58 +0100

    finito esercizio

---
+++ src/components/singleSeries.vue
@@ -0,0 +1,48 @@
+<script >
+    export default{
+        data() {
+            return {
+                
+            };
+        },
+        props:{
+            image:String,
+            name:String,
+        }
+    }
+</script>
+
+<template>
+    <div class="single-product">
+        <div>
+            <img :src="image" :alt="name">
+        </div>
+        <h2>
+            {{ name }}
+        </h2>
+
+    </div>
+</template>
+
+<style lang="scss">
+
+.single-product{
+    width: calc((100% - 40px) / 6);
+    height: 200px;
+    padding: 20px;
+    margin-top: 20px;
+    > div{
+        height: 150px;
+        width: 150px;
+        overflow: hidden;
+        
+
+    }
+    > h2{
+        color: white;
+        margin-top: 10px;
+        font-size: 1rem;
+    }
+}
+</style>
+
